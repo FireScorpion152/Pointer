722982a2e2e54d8d66de8624a0815c48f993553b
yumin <yumin.dima@gmail.com>
2024-05-14 20:49:13 +0300

    Add project files.

---
+++ DynamicMemory/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n = 5;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
+}
+void FillRand(int arr[], const int n)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+
+	}
+	cout << endl;
+}
