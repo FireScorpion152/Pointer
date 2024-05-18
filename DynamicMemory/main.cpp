57caf5d253400229db1787bb54194c837b47fd1e
yumin <yumin.dima@gmail.com>
2024-05-18 19:29:37 +0300

    Push done

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include<stack>
 using namespace std;
 using std::cin;
 using std::cout;
@@ -8,8 +9,12 @@ using std::endl;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
-int* push_back(int arr[], int& n, const int value);
 
+int* push_back(int arr[], int& n, const int value);
+int* push_front(int arr[], int& n, const int value);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
 
 void main()
 {
@@ -25,6 +30,16 @@ void main()
 	cout << "Введите добавляемое значение: "; cin >> value;
 	arr = push_back(arr, n, value);
 	Print(arr, n++);
+
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
 
 	delete[] arr;
 }
@@ -61,4 +76,36 @@ int* push_back(int arr[], int& n, const int value)
 	arr[n] = value;
 	n++;
 	return arr;
+}
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}
+	delete[] arr;
+	return buffer;
 }
